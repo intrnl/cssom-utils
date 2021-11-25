41346a7b5127d11e9d792df92e4da547287fad1c
intrnl <intrnl28+git@gmail.com>
2021-11-25 22:09:56 +0700

    readd missing min/max operations

--- src/math.js
+++ src/math.js
@@ -1,6 +1,11 @@
-const create = (op) => (...values) => `calc(${values.join(` ${op} `)})`;
+const createCalc = (op) => (...values) => `calc(${values.join(` ${op} `)})`;
+const createMath = (op) => (...values) => `${op}(${values.join(', ')})`;
 
-export const add = create('+');
-export const sub = create('-');
-export const div = create('/');
-export const mul = create('*');
+export const add = createCalc('+');
+export const sub = createCalc('-');
+export const div = createCalc('/');
+export const mul = createCalc('*');
+
+export const min = createMath('min');
+export const max = createMath('max');
+export const minmax = createMath('minmax');
